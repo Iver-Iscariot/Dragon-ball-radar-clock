53ff3879a249fa5c9772c500f101925ac3b3f0b2
Iver-Iscariot <iversobakk@hotmail.com>
2025-03-15 20:44:40 +0100

    Working demo
    Last one had wrong lookuptable for outer ring, and minutes and hour switched around

--- clock/clock.ino
+++ clock/clock.ino
@@ -2,24 +2,22 @@
   Dragon-radar clock
   Script written by Iver Iscariot Søbakk, for his custom PCB with an Atmega328 (open source at the github you found this file)
 */
+#include <TimeLib.h>
+
 
 int PWM = 9;        // the PWM pin the LED is attached to
 
-int A = 14;         // Hour cathode 1
-int B = 15;         // Hour cathode 2
-int C = 16;         // Hour anode 1
-int D = 17;         // Hour anode 2
+int A = 14; int B = 15;  // Minute cathode 
+int C = 16; int D = 17;  // Minute anode 
 
-int E = 18;         // minute cathode 1
-int F = 19;         // minute cathode 2
-int G = 6 ;         // minute anode 1
-int H = 7 ;         // minute anode 2
+int E = 18; int F = 19;  // Hour cathode 
+int G = 6 ; int H = 7 ;  // Hour anode
 
-int brightness = 0;  // how bright the LED is
-int fadeAmount = 5;  // how many points to fade the LED by
+int brightness = 0;  // Current brightness
+int fadeAmount = 5;  // Fading increase amount (fadespeed)
 
 // Define the HOUR/MIN pin states to select the correct LED from the two "matrix"es (see schematic)
-const byte pinStates[12][4] = {
+const byte pinStatesH[12][4] = {
     {0, 0, 1, 1}, // 1 -> 0011
     {0, 0, 1, 0}, // 2 -> 0010
     {0, 0, 0, 1}, // 3 -> 0001
@@ -33,11 +31,26 @@ const byte pinStates[12][4] = {
     {1, 0, 0, 1}, // 11 -> 1001
     {1, 0, 0, 0}  // 12 -> 1000
 };
+const byte pinStates[12][4] = {
+    {0, 0, 1, 1}, // 1  -> 0011
+    {0, 0, 0, 1}, // 2  -> 0001
+    {0, 0, 1, 0}, // 3  -> 0010
+    {0, 0, 0, 0}, // 4  -> 0000
+    {1, 0, 1, 1}, // 5 -> 1011
+    {1, 0, 0, 1}, // 6 -> 1001
+    {1, 0, 1, 0}, // 7 -> 1010
+    {1, 0, 0, 0},  // 8 -> 1000
+    {0, 1, 1, 1}, // 9  -> 0111
+    {0, 1, 0, 1}, // 10 -> 0101
+    {0, 1, 1, 0}, // 11 -> 0110
+    {0, 1, 0, 0} // 12 -> 0100
+};
 
-int mincounter = 1;
+int mincounter = 0;
 int houcounter = 1;
 
-void setHour(int inputNumber) {    
+void setMinute(int inputNumber) {    
+    inputNumber = inputNumber/5 + 1; // divides to the nearest 5 minutes, rounding up, so yu have more time than you think 4 -> 0+1, lighting the "5 min" led
     if (inputNumber < 1 || inputNumber > 12) return; // Ensure valid range
     
     digitalWrite(A, pinStates[inputNumber - 1][0]);
@@ -46,14 +59,13 @@ void setHour(int inputNumber) {
     digitalWrite(D, pinStates[inputNumber - 1][3]);
 }
 
-void setMinute(int inputNumber) {    
-    inputNumber = inputNumber/5 + 1; // divides to the nearest 5 minutes, rounding up, so yu have more time than you think 4 -> 0+1, lighting the "5 min" led
+void setHour(int inputNumber) {    
     if (inputNumber < 1 || inputNumber > 12) return; // Ensure valid range
     
-    digitalWrite(E, pinStates[inputNumber - 1][0]);
-    digitalWrite(F, pinStates[inputNumber - 1][1]);
-    digitalWrite(G, pinStates[inputNumber - 1][2]);
-    digitalWrite(H, pinStates[inputNumber - 1][3]);
+    digitalWrite(E, pinStatesH[inputNumber - 1][3]);
+    digitalWrite(F, pinStatesH[inputNumber - 1][2]);
+    digitalWrite(G, pinStatesH[inputNumber - 1][1]);
+    digitalWrite(H, pinStatesH[inputNumber - 1][0]);
 }
 
 
@@ -61,44 +73,33 @@ void setMinute(int inputNumber) {
 void setup() {
   pinMode(PWM, OUTPUT);
   
-  pinMode(A, OUTPUT);
-  pinMode(B, OUTPUT);
-  pinMode(C, OUTPUT);
-  pinMode(D, OUTPUT);
+  pinMode(A, OUTPUT); pinMode(B, OUTPUT); pinMode(C, OUTPUT); pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT); pinMode(F, OUTPUT); pinMode(G, OUTPUT); pinMode(H, OUTPUT);
 
-  pinMode(E, OUTPUT);
-  pinMode(F, OUTPUT);
-  pinMode(G, OUTPUT);
-  pinMode(H, OUTPUT);
-
-  
-  digitalWrite(A, LOW);  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(B, LOW);  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(C, LOW);  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(D, LOW);  // turn the LED on (HIGH is the voltage level)
-
-  digitalWrite(E, LOW);  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(F, LOW);  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(G, LOW);  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(H, LOW);  // turn the LED on (HIGH is the voltage level)
+  // Dont know if this is strictly necesarry
+  digitalWrite(A, LOW);  digitalWrite(B, LOW);  digitalWrite(C, LOW);  digitalWrite(D, LOW);  
+  digitalWrite(E, LOW);  digitalWrite(F, LOW);  digitalWrite(G, LOW);  digitalWrite(H, LOW);  
 
   // just for circle example
-  analogWrite(PWM, 127);   // half brightness
+  analogWrite(PWM, 4);   // half brightness
 
 
   Serial.begin(9600);
+
+
+  hourFormat12(); 
 }
 
 
 void loop() {
   setHour(houcounter);
-  setMinute(mincounter*5);
+  setMinute(mincounter);
 
-  delay(100);
+  delay(50);
 
   mincounter += 1;
-  if (mincounter == 13){  // if all the way round
-    mincounter = 1;      // start over
+  if (mincounter == 60){  // if all the way round
+    mincounter = 0;      // start over
     houcounter += 1;     // increment hour
   }
   if (houcounter == 13){  // iff all the way round
